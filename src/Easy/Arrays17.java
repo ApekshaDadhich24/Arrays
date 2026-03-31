4897b92b1d8c21b64dc64e45ba3b3bca17e30fb3
ApekshaDadhich24 <apekshadadhich24@gmail.com>
2026-03-31 18:27:24 +0530

    maximum consecutive ones

---
+++ src/Easy/Arrays17.java
@@ -0,0 +1,26 @@
+//Maximum consecutive ones
+
+package Easy;
+
+public class Arrays17 {
+
+    static void maxConsecutive(int [] nums){
+        int maxi=0;
+        int count  = 0 ;
+        for (int i =0 ; i < nums.length ; i ++){
+            if(nums[i]==1) {
+                count++;
+                maxi = Math.max(maxi, count);
+            }else {
+                count =0;
+            }
+        }
+        System.out.println(maxi);
+
+    }
+
+    public static void main(String[] args) {
+        int[]nums = {1,1,0,1,1,1,1,0,1,1,1};
+        maxConsecutive(nums);
+    }
+}
