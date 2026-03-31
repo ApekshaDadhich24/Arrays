394d987429a411b36191e24507b992cc768629a9
ApekshaDadhich24 <apekshadadhich24@gmail.com>
2026-03-31 19:03:06 +0530

    maximum consecutive ones - using xor

---
+++ src/Easy/Arrays18.java
@@ -0,0 +1,18 @@
+//max consecutive ones -- most optimal appraoch
+
+package Easy;
+public class Arrays18 {
+
+    static int maxConsecutive(int[]nums){
+        int xor = 0;
+        for(int i = 0 ; i < nums.length ; i++){
+            xor = xor^nums[i];
+        }return xor;
+    }
+
+    public static void main(String[] args) {
+        int []nums ={1,1,2,2,3,4,4,5,5};
+        int result= maxConsecutive(nums);
+        System.out.println(result);
+    }
+}
