252df6334d53d2d035d098bf5689d7a6cf53728e
ApekshaDadhich24 <apekshadadhich24@gmail.com>
2026-04-01 21:58:36 +0530

    two sum using hashmap

---
+++ src/Medium/Array01.java
@@ -0,0 +1,27 @@
+//two sum using hashmap
+
+package Medium;
+import java.util.*;
+public class Array01 {
+
+    static void twoSum(int[]nums, int target){
+        Map<Integer, Integer> map = new HashMap<>();
+        for(int i = 0 ; i < nums.length ; i++){
+            int more = target - nums[i];
+            if (map.containsKey(more)){
+                System.out.println(map.get(more) + " " + i);
+                return;
+            }
+            map.put(nums[i],i);
+        }
+
+    }
+
+    public static void main(String[] args) {
+        int[]nums = {2,6,5,8,11};
+        int target = 14;
+        twoSum(nums, target);
+
+    }
+
+}
