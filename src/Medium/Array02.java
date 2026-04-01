0e0abe4ee2ce020a1f975d0a1f37dcbd0a2422ae
ApekshaDadhich24 <apekshadadhich24@gmail.com>
2026-04-01 22:03:40 +0530

    two sum using two pointer

---
+++ src/Medium/Array02.java
@@ -0,0 +1,32 @@
+// two sum - most optimal approach
+
+package Medium;
+import java.util.*;
+public class Array02 {
+
+    static void twoSum(int[] nums, int target){
+        Arrays.sort(nums);
+        int left = 0;
+        int right = nums.length - 1;
+        while(left < right){
+            int sum = nums[left] + nums[right];
+            if(sum == target){
+                System.out.println(nums[left] + " " + nums[right]);
+                return;
+                }
+            else if(sum < target){
+                left++;
+            }
+            else{
+                right--;
+            }
+        }
+    }
+    public static void main(String[] args) {
+        int[] nums = {2,6,5,8,11};
+        int target = 14;
+        twoSum(nums, target);
+    }
+}
+
+
