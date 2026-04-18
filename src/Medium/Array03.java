ac9f1b8fd494cc7da182bcd6709aea43ff4758b9
ApekshaDadhich24 <apekshadadhich24@gmail.com>
2026-04-18 18:28:06 +0530

    sort the number of 0's,1's and 2's in the array

---
+++ src/Medium/Array03.java
@@ -0,0 +1,27 @@
+// sort an arrays of 0's , 1's and 2's.
+
+package Medium;
+import java.util.Arrays;
+public class Array03 {
+
+    static int [] sortArray(int []arr){
+        int count0=0;
+        int count1=0;
+        int count2 =0 ;
+        for (int i = 0 ; i < arr.length ; i++){
+            if (arr[i]==0){count0++;}
+            if (arr[i]==1){count1++;}
+            if (arr[i]==2){count2++;}
+        }
+        for (int i = 0 ; i < count0 ; i++){arr[i]=0;}
+        for (int i = count0 ; i < count0+count1 ; i++){arr[i]=1;}
+        for (int i = count0+count1 ; i < arr.length ; i++){arr[i]=2;}
+        return arr;
+    }
+
+    public static void main(String[] args) {
+        int arr[]={0,1,2,0,1,2,1,2,0,0,0,1};
+        int result[] = sortArray(arr);
+        System.out.println(Arrays.toString(result));
+    }
+}
